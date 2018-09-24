cedad4a1eec07f72ce68097ccb044fa35c2f6fbf
Danny <dinesh.kumar@sntinfotech.com>
2018-09-24 15:08:28 +0530

    update18

--- app/src/main/java/com/foodapp/murti/Activity/Login.java
+++ app/src/main/java/com/foodapp/murti/Activity/Login.java
@@ -1,8 +1,16 @@
 package com.foodapp.murti.Activity;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -48,6 +56,9 @@ public class Login extends AppCompatActivity  {
     Spinner city;
     ArrayList<Getseter> DataList=new ArrayList<>();
     List<String> City_Names = new ArrayList<String>();
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    String otpString;
+    EditText otp_edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +78,9 @@ public class Login extends AppCompatActivity  {
         Getseter.preferences = getSharedPreferences("My_prefence", MODE_PRIVATE);
         Getseter.editor =Getseter.preferences.edit();
 
+        if (checkAndRequestPermissions()) {
+            // carry on the normal flow, as the case of  permissions  granted.
+        }
 
         forgotssword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -373,7 +387,7 @@ public class Login extends AppCompatActivity  {
         dialog2.setContentView(R.layout.otp_dialog_verfy);
 //        dialog2.setCancelable(false);
 
-        final EditText otp_edit= (EditText) dialog2.findViewById(R.id.otp_edit);
+        otp_edit= (EditText) dialog2.findViewById(R.id.otp_edit);
         final EditText newpass= (EditText) dialog2.findViewById(R.id.newpass);
         final EditText newpass2= (EditText) dialog2.findViewById(R.id.newpass2);
         //TextView recieve= (TextView) dialog2.findViewById(R.id.recieve);
@@ -486,5 +500,62 @@ public class Login extends AppCompatActivity  {
     }
 
 
+    private  boolean checkAndRequestPermissions() {
+        int permissionSendMessage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
 
+        int receiveSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS);
+
+        int readSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_SMS);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (receiveSMS != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.RECEIVE_MMS);
+        }
+        if (readSMS != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_SMS);
+        }
+        if (permissionSendMessage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        LocalBroadcastManager.getInstance(this).
+                registerReceiver(receiver, new IntentFilter("otp"));
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equalsIgnoreCase("otp")) {
+                final String message = intent.getStringExtra("message");
+
+//                TextView tv = (TextView) findViewById(R.id.txtview);
+//                tv.setText(message);
+
+                Log.d("sgdfgdfgdfgdfd",message);
+                Log.d("gsdfgsdfdgvd",message.substring(message.length()-8));
+                otpString=message.substring(message.length()-8).replaceAll("\\s+","");
+                otp_edit.setText(otpString);
+            }
+        }
+    };
 }
